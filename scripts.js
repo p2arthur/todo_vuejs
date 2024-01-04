6455457f0b28e81ed4e020cdc7e10f486ce41190
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-03 22:05:44 -0300

    (feat) started implementing local storage to the todo using vue

--- scripts.js
+++ scripts.js
@@ -2,16 +2,39 @@ const todoApp = {
   data() {
     return { todo_list: [], newTodo: { text: null, done: false } };
   },
+
+  //Lifecycle hooks
+  //----------------------------------------------------------------------------
+  beforeCreate() {
+    console.log('before create');
+  },
+
+  created() {
+    const todo_list = localStorage.getItem('todo_list');
+
+    this.todo_list = todo_list ? JSON.parse(todo_list) : this.todo_list;
+  },
+
+  beforeUpdate() {
+    console.log('before update');
+  },
+  //----------------------------------------------------------------------------
   methods: {
     addTodo: function () {
-      console.log(this.newTodo);
-      console.log('addTodo');
-      this.todo_list.push(this.newTodo);
+      if (this.newTodo.text) {
+        this.todo_list.push(this.newTodo);
+        this.newTodo = {
+          done: false,
+        };
+        localStorage.setItem('todo_list', JSON.stringify(this.todo_list));
+      } else {
+        alert('To-do text is required');
+      }
     },
 
     toggleTodo: function (item) {
       item.done = !item.done;
-      console.log('toggleTodo');
+      console.log(item.done);
     },
 
     removeAllTodos: function () {
